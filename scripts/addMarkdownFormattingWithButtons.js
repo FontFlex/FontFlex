310236d8dc87c050e9b2baf3ef75020eeb855899
Jayce Sagvold <97198910+jasjs1@users.noreply.github.com>
2023-06-22 14:43:42 -0500

    Create addMarkdownFormattingWithButtons.js

---
+++ scripts/addMarkdownFormattingWithButtons.js
@@ -0,0 +1,20 @@
+const markdownTextEditor = document.getElementById("markdown-editor");
+const previewMarkdownChanges = document.getElementById("preview"); // May not use
+
+function addH1() {
+    const markdownTextEditor = document.getElementById("markdown-editor");
+
+    markdownTextEditor.innerHTML = "# ";
+}
+
+function addH2() {
+    const markdownTextEditor = document.getElementById("markdown-editor");
+
+    markdownTextEditor.innerHTML = "## ";
+}
+
+function addH3() {
+    const markdownTextEditor = document.getElementById("markdown-editor");
+
+    markdownTextEditor.innerHTML = "### ";
+}
